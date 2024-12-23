f8644196927dd02bc143150fbaf556ddb2dabee8
Daisho Komiyama <daishokomiyama@gmail.com>
2024-12-23 12:08:04 -0500

    Able to add raw text to store object

--- client/components/PlayPage/PlayPage.js
+++ client/components/PlayPage/PlayPage.js
@@ -29,6 +29,11 @@ export default class PlayPage extends HTMLElement {
   #encryptButton = null
 
   /**
+   * @type {HTMLElement | null}
+   */
+  #aside = null
+
+  /**
    * @type {HTMLButtonElement | null}
    */
   #sidebarButton = null
@@ -47,17 +52,14 @@ export default class PlayPage extends HTMLElement {
   }
 
   connectedCallback() {
-    if (document.startViewTransition) {
-      document.startViewTransition(() => {
-        this.#init()
-      })
-    } else {
-      this.#init()
-    }
+    document.startViewTransition
+      ? document.startViewTransition(() => this.#render())
+      : this.#render()
+
     this.#setupSubmit()
   }
 
-  #init() {
+  #render() {
     this.#importButton = document.$el("button")
     this.#encryptButton = document.$el("button")
     this.#sidebarButton = document.$el("button")
@@ -66,12 +68,14 @@ export default class PlayPage extends HTMLElement {
     this.#encryptButton.innerText = "encrypt"
     this.#sidebarButton.classList.add("icon-btn")
 
+    const urlLabel = document.$el("label")
     const urlInput = document.$el("input")
     urlInput.$attr("type", "url")
-    const urlLabel = document.$el("label")
+    urlInput.$attr("name", "url")
     urlLabel.innerText = "url"
-    const nameInput = document.$el("input")
     const nameLabel = document.$el("label")
+    const nameInput = document.$el("input")
+    urlInput.$attr("name", "name")
     nameLabel.innerText = "name (optional)"
 
     const urlFieldset = document.$el("fieldset")
@@ -80,28 +84,84 @@ export default class PlayPage extends HTMLElement {
     nameFieldset.append(nameLabel, nameInput)
 
     const cancelButton = document.$el("button")
+    cancelButton.$attr("type", "button")
     cancelButton.innerText = "cancel"
     const addButton = document.$el("button")
+    addButton.$attr("type", "button")
     addButton.innerText = "add"
 
-    const form = document.$el("form")
-    form.append(urlFieldset, nameFieldset, cancelButton, addButton)
-
-    const aside = document.$el("aside")
-    aside.append(this.#sidebarButton)
-    aside.append(form)
+    addButton.$on(
+      "click",
+      () => {
+        const value = urlInput.value.trim()
+        if (!value) return
+        app.store.rawText = [value, ...app.store.rawText]
+        urlInput.value = ""
+      },
+    )
 
     const div = document.$el("div")
     div.classList.add("top-nav")
     div.append(this.#importButton, this.#encryptButton)
 
+    const form = document.$el("form")
+    form.append(urlFieldset, nameFieldset, cancelButton, addButton)
+
+    this.#aside = document.$el("aside")
+    this.#aside.append(this.#sidebarButton)
+    this.#aside.append(form)
+
     const section = this.root.$("section")
     if (!(section instanceof HTMLElement)) return
-    section.append(div, aside)
+    section.append(div, this.#aside)
 
-    this.#sidebarButton.$on("click", () => {
-      aside.classList.toggle("active")
+    this.#sidebarButton?.$on("click", () => {
+      this.#aside?.classList.toggle("active")
     })
+
+    this.#setSubmit()
+  }
+
+  // #populateList() {
+  //   // const document.$el("textarea")
+  //   document.$on("addrawtext", () => {
+  //     console.log("==> ADDED", app.store.rawText)
+  //   })
+  // }
+
+  #setSubmit() {
+    console.log("==> NOPE?", this.#encryptButton)
+    this.#encryptButton?.$on("click", async () => {
+      console.log("==>", app.store.rawText)
+      const response = await fetch("/encrypt", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(app.store.rawText),
+      })
+      console.log("==> RES", await response.text())
+      // const urlInput = this.root.$('input[name="url"]')
+      // if (!(urlInput instanceof HTMLTextAreaElement)) return
+      // textarea.classList.add("output")
+      // textarea.value = await response.text()
+
+      // this.root.$("section")?.append(textarea)
+      this.#renderCopyButton()
+    })
+  }
+
+  #renderCopyButton() {
+    const btn = document.$el("button")
+    btn.textContent = "Copy to clipboard"
+    btn.$on("click", () => {
+      const textarea = this.root.$("textarea.output")
+      if (!(textarea instanceof HTMLTextAreaElement)) return
+      navigator.clipboard.writeText(textarea.value)
+        .then(() => console.log("Copied successfully!"))
+        .catch((err) => console.error("Copy failed:", err))
+    })
+    this.root.$("section")?.append(btn)
   }
 
   /**
